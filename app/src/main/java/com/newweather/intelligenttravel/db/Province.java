a3b5a146c845ddba08be50bc209588f0a6e6ee62
pipe <pwpiepdream@gamil.com>
2019-06-09 13:25:06 +0800

    完善首页功能

---
+++ app/src/main/java/com/newweather/intelligenttravel/db/Province.java
@@ -0,0 +1,27 @@
+package com.newweather.intelligenttravel.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
--- app/src/main/java/com/newweather/intelligenttravel/util/Utility.java
+++ app/src/main/java/com/newweather/intelligenttravel/util/Utility.java
@@ -1,10 +1,16 @@
 package com.newweather.intelligenttravel.util;
 
 
+import android.text.TextUtils;
+
 import com.google.gson.Gson;
 import com.newweather.intelligenttravel.Entity.Subway;
 import com.newweather.intelligenttravel.Entity.Train;
+import com.newweather.intelligenttravel.db.City;
+import com.newweather.intelligenttravel.db.Province;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -18,7 +24,7 @@ public class Utility {
             JSONObject jsonObject=new JSONObject(response);
             String trainContent=jsonObject.toString();
             //Log.d("ac",trainContent);
-            return new Gson().fromJson(trainContent,Train.class);
+            return new Gson().fromJson(trainContent, Train.class);
 
         }catch (Exception e){
             e.printStackTrace();
@@ -33,12 +39,53 @@ public class Utility {
             String subwayContent=jsonObject.toString();
             //Log.d("Util",subwayContent);
 
-            return new Gson().fromJson(subwayContent,Subway.class);
+            return new Gson().fromJson(subwayContent, Subway.class);
 
         }catch (Exception e){
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * 获取省份和城市
+     */
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public static boolean handleCityResponse(String response, int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCities = new JSONArray(response);
+                for(int i = 0;i<allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 
 }
